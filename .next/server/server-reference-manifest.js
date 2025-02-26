ae87bdd594b6b0c32d0e79d4aebea97739af12ad
simulasikode <simulasikode@gmail.com>
2025-02-26 20:16:41 +0700

    chore: add all

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pfQS4DB2Ke1wz5VWH1fGq3L+mDM+Ki2mSYdYxGPPqOE=\"\n}"
